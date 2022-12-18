f996deaef63b09c2ccbb3a63e3510a622895073b
Te <mammoth2000@proton.me>
2022-12-18 22:51:46 +0000

    mintdata

---
+++ contracts/26_MintData.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.17;
+
+// libs
+import "contracts/libs/Whitelist.sol";
+import "contracts/libs/SafeMath.sol";
+
+
+contract mintData is Whitelist {
+
+}
